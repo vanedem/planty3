0386e8f0af768342c81d8e44b79327e2ffe1eec9
vanedem <vanessa.demaecker@gmail.com>
2024-04-20 16:00:59 +0200

    MAJ 20/04 à 16h

---
+++ wp-content/themes/astra-child/functions.php
@@ -0,0 +1,35 @@
+<?php
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
+
+// BEGIN ENQUEUE PARENT ACTION
+// AUTO GENERATED - Do not modify or remove comment markers above or below:
+
+if ( !function_exists( 'chld_thm_cfg_locale_css' ) ):
+    function chld_thm_cfg_locale_css( $uri ){
+        if ( empty( $uri ) && is_rtl() && file_exists( get_template_directory() . '/rtl.css' ) )
+            $uri = get_template_directory_uri() . '/rtl.css';
+        return $uri;
+    }
+endif;
+add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
+        
+if ( !function_exists( 'child_theme_configurator_css' ) ):
+    function child_theme_configurator_css() {
+        wp_enqueue_style( 'chld_thm_cfg_child', trailingslashit( get_stylesheet_directory_uri() ) . 'style.css', array( 'astra-theme-css' ) );
+    }
+endif;
+add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 10 );
+
+// END ENQUEUE PARENT ACTION
+function ajouter_lien_menu_principal($items, $args) {
+    
+    if (is_user_logged_in()) {
+        //si oui, on affiche le lien admin
+        $items .= '<li><a href="'. get_admin_url() .'">Admin</a></li>';
+    }
+    return $items;
+}
+
+// Ajouter le hook pour exécuter la fonction
+add_filter('wp_nav_menu_items', 'ajouter_lien_menu_principal', 10, 2);
